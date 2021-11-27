443123e3d661145aa8ac8aab6f3ff8322eb8ea4b
williamjackson314 <wtjackson314@gmail.com>
2021-11-27 15:37:11 -0500

    Implement functions to read strings from a file, find length of lcs, and print the lcs.

--- main.py
+++ main.py
@@ -1,1 +1,109 @@
+def read_file(filename):
+    """
 
+    Parameters
+    ----------
+    filename : string
+        The name of the file to read from.
+
+    Returns
+    -------
+    lines : list
+        List of lines from the file.
+
+    """
+
+    f = open(filename, "r")
+    lines = f.readlines()
+    lines = [x.rstrip('\n') for x in lines]
+
+    return lines
+
+def lcs_length(x, y):
+    """
+
+    Parameters
+    ----------
+    x : string
+        String to find subsequence relative to.
+
+    y : string
+        String to find subsequence relative to.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    # holds the length of lcs
+    len_lcs = 0
+
+    # create two empty matrices
+    c = []
+    b = []
+
+    # number of rows in matrix c (minus padding) and b
+    m = len(x)
+
+    # number of columns in matrix c (minus padding) and b
+    n = len(y)
+
+    c = [[0] * (n + 1) for i in range(m + 1)]
+    b = [[None] * (n) for i in range(m)]
+
+    for i in range(m + 1):
+        c[i][0] = 0
+
+    for j in range(1, n + 1):
+        c[0][j] = 0
+
+    for i in range(m):
+        for j in range(n):
+
+            if x[i] == y[j]:
+                c[i][j] = c[i - 1][j - 1] + 1
+                b[i][j] = '\\' # '\' ignoring the escape character
+                if (c[i][j] > len_lcs):
+                    len_lcs = c[i][j]
+
+            elif c[i - 1][j] >= c[i][j - 1]:
+                c[i][j] = c[i - 1][j]
+                b[i][j] = '|'
+
+            else: # c[i][j - 1] > c[i - 1][j]
+                c[i][j] = c[i][j - 1]
+                b[i][j] = '_'
+
+    return c, b, len_lcs
+
+def print_lcs(b, x, i, j):
+    if (i < 0) or (j < 0):
+        return
+
+    if (b[i][j] == '\\'):
+        print_lcs(b, x, i - 1, j - 1)
+        print(x[i], end='')
+
+    elif (b[i][j] == '|'):
+        print_lcs(b, x, i - 1, j)
+
+    elif (b[i][j] == '_'):
+        print_lcs(b, x, i, j - 1)
+
+def main():
+    strings = read_file("dna.txt")
+
+    x = strings[0]
+    y = strings[1]
+    c, b, length = lcs_length(x, y)
+
+    # print the length of the lcs
+    print(length)
+
+    # print the lcs between two entire strings
+    print_lcs(b, x, len(x) - 1, len(y) - 1)
+
+    return None
+
+main()
