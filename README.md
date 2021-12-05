3dcc30220cc91c298a39c583165ad8911296882e
williamjackson314 <wtjackson314@gmail.com>
2021-12-05 13:48:02 -0500

    Update README. Remove redundant code in lcs_length function.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# LongestCommonSubsequence
+# Notes
+- Written using Python 3.7
+- Function descriptions also found in Docstrings in each function
+
+# Requirements
+- Libraries: None
+- Files: to compare strings from file the file must be a txt file with the two strings as the first two lines of the file. File must be in the same
+folder as main.py
+
+# Longest Common Subsequence
+
+**lcs_length**
+- This function finds both the longest common subsequence for each section of two strings and the length of the
+longest common subsequence of both strings in their entirety.
+
+Longest Common Subsequence Length
+- 20
+
+Longest Common Subsequence
+- GTCGTCGGAAGCCGGCCGAA
+
+**print_lcs**
+- In order for this function to work, the lcs_length (above) must be called first, with the two strings that the lcs is being printed
+from as arguments.
+- This function prints the longest common subsequence between two strings, ending at character 'i' for the first and character 'j' for the second 
+(i and j are function parameters).
--- main.py
+++ main.py
@@ -32,16 +32,16 @@ def lcs_length(x, y):
 
     Returns
     -------
-    None.
+    c : matrix holding lcs lengths
+
+    b : matrix holding indicator characters to reconstruct the lcs with later
+
+    lcs_len : the length of the longest common subsequence
 
     """
 
     # holds the length of lcs
     len_lcs = 0
-
-    # create two empty matrices
-    c = []
-    b = []
 
     # number of rows in matrix c (minus padding) and b
     m = len(x)
@@ -52,12 +52,6 @@ def lcs_length(x, y):
     c = [[0] * (n + 1) for i in range(m + 1)]
     b = [[None] * (n) for i in range(m)]
 
-    for i in range(m + 1):
-        c[i][0] = 0
-
-    for j in range(1, n + 1):
-        c[0][j] = 0
-
     for i in range(m):
         for j in range(n):
 
@@ -67,6 +61,7 @@ def lcs_length(x, y):
                 if (c[i][j] > len_lcs):
                     len_lcs = c[i][j]
 
+            # Notice: when a tie occurs, the algorithm chooses the cell above the current one to reference
             elif c[i - 1][j] >= c[i][j - 1]:
                 c[i][j] = c[i - 1][j]
                 b[i][j] = '|'
@@ -78,6 +73,27 @@ def lcs_length(x, y):
     return c, b, len_lcs
 
 def print_lcs(b, x, i, j):
+    """
+
+    Parameters
+    ----------
+    b : matrix (2-D list)
+        Holds indicator characters used to print the lcs.
+
+    x : string
+        string to print lcs from.
+
+    i : int
+        Starting row in b matrix.
+
+    j : int
+        Starting column in b matrix.
+
+    Returns
+    -------
+    None.
+
+    """
     if (i < 0) or (j < 0):
         return
 
@@ -92,18 +108,22 @@ def print_lcs(b, x, i, j):
         print_lcs(b, x, i, j - 1)
 
 def main():
+
+    # Read in lines from text file
     strings = read_file("dna.txt")
 
+    # store first two lines as strings to find lcs of
     x = strings[0]
     y = strings[1]
+
+    # Find lcs and lcs length
     c, b, length = lcs_length(x, y)
 
     # print the length of the lcs
-    print(length)
+    print("LCS Length = " + str(length))
 
-    # print the lcs between two entire strings
+    # print the lcs between two strings from file
+    print("LCS = ", end='')
     print_lcs(b, x, len(x) - 1, len(y) - 1)
-
-    return None
 
 main()
